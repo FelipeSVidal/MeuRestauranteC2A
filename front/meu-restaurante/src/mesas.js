f1b9df8d85470d6efff5686f504db071608ff8b9
felipe sousa <felipedesousavidal@gmail.com>
2021-01-04 16:49:30 -0300

    front ended

--- front/meu-restaurante/src/mesas.js
+++ front/meu-restaurante/src/mesas.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import {useParams, Link} from 'react-router-dom'
+import api from './api'
 
 
 const Mesas = (props) => {
   const [mesas, setMesas] = React.useState([]);
 
+  const getMesas = React.useCallback( async () => {
+    const response = await api.get("/mesas");
+    console.log(response);
+    const data = await response.json();
+    setMesas(data);
+  }, []);
+
   React.useEffect( () => {
-    (async () => {
-      const response = await fetch("/mesas");
-      const data = await response.json();
-      setMesas(data);
-    })
+    getMesas();
   }, []);
 
 
   return (
     <>
-      {data.map( (m,i) => 
+      {mesas.map( (m,i) => 
         <Link key={i} to={`pedidos/${m._id}`}>Mesa {m.numero} - {m.status}</Link> 
       )}
     </>
